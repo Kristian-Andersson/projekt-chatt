5e86e2412e6b62bd5f0795d09ee5132b63b5848a
Mallarden <antonanderson.com@gmail.com>
2018-03-08 15:56:33 +0100

    inlogg popup

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,1 +1,21 @@
-console.log("test");
+var attempt = 3; 
+
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "admin" && password == "admin") {
+        window.location = "index.html"; 
+        return false;
+    } else {
+        attempt--; 
+        alert("You have " + attempt + " attempts left");
+
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
