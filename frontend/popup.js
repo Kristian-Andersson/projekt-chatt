7b90294145ca542d4af261bfb7e1432ff3d79cf6
Mallarden <antonanderson.com@gmail.com>
2018-03-08 16:14:12 +0100

    Inlogg styling

--- frontend/popup.js
+++ frontend/popup.js
@@ -2,7 +2,7 @@ var attempt = 3;
 
 function validate() {
     var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+    var password = document.getElementById("email").value;
     if (username == "admin" && password == "admin") {
         window.location = "index.html"; 
         return false;
@@ -12,10 +12,18 @@ function validate() {
 
         if (attempt == 0) {
             document.getElementById("username").disabled = true;
-            document.getElementById("password").disabled = true;
+            document.getElementById("email").disabled = true;
             document.getElementById("submit").disabled = true;
             return false;
         }
     }
 }
+
+var input = document.getElementById("email");
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("submit").click();
+    }
+});
 
