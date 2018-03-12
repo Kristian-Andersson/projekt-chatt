4b5d1419d73222db8c6552ae9702683a651904aa
Kristian-Andersson <krit_ian@hotmail.se>
2018-03-12 16:11:47 +0100

    Lagt till ett inputfält som skickar texten till express.

--- backend/populate.js
+++ backend/populate.js
@@ -1,13 +1,9 @@
-var express = require('express');
-var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var ObjectID = require('mongodb').ObjectID;
 var fetchJson = require('node-fetch-json');
 var path = require('path');
 var db;
-var app = express();
 
-app.use(bodyParser.json());
 
 MongoClient.connect('mongodb://localhost:27017', function(error, client) {
   if (error) {
@@ -16,55 +12,43 @@ MongoClient.connect('mongodb://localhost:27017', function(error, client) {
   } else {
     console.log("Connected to database!");
     db = client.db('chattdb');
+
+
+    // Tar bort alla värdena i collectionen users
+    db.collection('users').remove({}, function (error, result) {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      } else {
+        response.send(result);
+      }
+    });
+
+
+    // Skapar lite fejkuppgifter för testning
+    db.collection('users').insertMany([{
+      "firstName": "Kristian",
+      "surname": "Andersson",
+      "username": "kirre253",
+      "messages": {
+        "2018-03-08, 14:25:52": "Hej på dig! :)",
+        "2018-03-08, 14:39:22": "Vad gör du?"
+      }
+    },
+    {
+      "firstName": "Robin",
+      "surname": "Lundström",
+      "username": "robban976",
+      "messages": {
+        "2018-03-08, 14:42:50": "Hejsan"
+      }
+    }], function (error, result) {
+      if (error) {
+        response.status(500).send(error);
+        return;
+      } else {
+        response.send(result);
+      }
+    });
   }
-});
-
-// /chatt är kopplad till frontend mappen
-app.use('/chatt', express.static(path.join(__dirname, 'frontend')));
-
-// Skapar lite fejkuppgifter för testning
-app.put('/', function (request, respone) {
-  db.collection('users').insertMany([{
-    "firstName": "Kristian",
-    "surname": "Andersson",
-    "username": "kirre253",
-    "messages": {
-      "2018-03-08, 14:25:52": "Hej på dig! :)",
-      "2018-03-08, 14:39:22": "Svara då för i helvete! >:("
-    }
-  },
-  {
-    "firstName": "Robin",
-    "surname": "Lundström",
-    "username": "robban1337_xXdragonSlayerXx",
-    "messages": {
-      "2018-03-08, 14:40:28": "Lugna ned dig din surballe..",
-      "2018-03-08, 14:42:50": "Hej"
-    }
-  }]), function (error, result) {
-    if (error) {
-      response.status(500).send(error);
-      return;
-    } else {
-      response.send(result);
-    }
-  };
-});
-
-
-// Hämtar alla fejkuppgifter och skickar dem till en array
-app.get('/', function (request, response) {
-  db.collection('users').find().toArray(function (error, result) {
-    if (error) {
-      response.status(500).send(error);
-      return;
-    } else {
-      response.send(result);
-    }
-  });
-});
-
-
-app.listen(3000, function () {
-  console.log('The server is running!')
 });
--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,37 @@
 
-ReactDOM.render(<div><input></input><input></input></div>, document.querySelector('#app'));
+class Chatten extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      inputMessage: null
+    };
+    this.onTextChange = this.onTextChange.bind(this);
+  }
+  onTextChange(event) {
+    this.setState({ inputMessage: event.target.value });
+  }
+  render() {
+    return <div className="chattwrapper">
+      <div className="chattbox"></div>
+      <input onChange={this.onTextChange}></input>
+      <button onClick={() => {
+        fetch('http://localhost:3000/', {
+          body: '{ "message": "' + this.state.inputMessage + '"}',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'POST'
+        }).then(function (response) {
+          return response.json();
+        }).then(function (result) {
+          console.log(result);
+        });
+      }}>Send</button>
+    </div>
+  }
+}
+
+ReactDOM.render(
+  <Chatten></Chatten>,
+  document.getElementById('app')
+ );
