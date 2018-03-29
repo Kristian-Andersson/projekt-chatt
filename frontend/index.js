691cf151da5fdf42539a371639bacdbfc9696d42
robin <rroobin98@hotmail.com>
2018-03-29 15:38:48 +0200

    commitar för att få det nyaste coderna kommenterade ut index.js atm

--- frontend/index.js
+++ frontend/index.js
@@ -1,28 +1,48 @@
 
+
+
+
+/*var Switch = ReactRouterDOM.Switch;
 var HashRouter = ReactRouterDOM.HashRouter;
 var Link = ReactRouterDOM.Link;
 var Route = ReactRouterDOM.Route;
 
+var Home = createReactClass({
+ render: function () {
+   return <h1>Home</h1>;
+ }
+});
 
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+var About = createReactClass({
+ render: function () {
+   return <h1>About</h1>;
+ }
+});
+
+var Contact = createReactClass({
+ render: function () {
+   return <h1>Contact</h1>;
+ }
+});
 
 
 
-ReactDOM.render(<HashRouter>
+  ReactDOM.render(<HashRouter>
 <div>
-  <nav>
-    <ul>
-      <li><Link to={"/index"}>home</Link></li>
-      <li><Link to={"/gruppchatt"}>Gruppchatt</Link></li>
-      <li><Link to={"/privatchatt"}>Privatchatt</Link></li>
-    </ul>
-  </nav>
-  <Route component={index} path="/index"></Route>
-  <Route component={gruppchatt} path="/gruppchatt"></Route>
-  <Route component={privatchatt} path="/privatchatt"></Route>
+<nav>
+  <ul>
+    <li><Link to="/inlogg">Inlogg</Link></li>
+    <li><Link to="/gruppchatt">Gruppchatt</Link></li>
+    <li><Link to="/privatchatt">Privatchatt</Link></li>
+
+  </ul>
+</nav>
+
+<Route component={Home} path="/home"></Route>
+<Route component={About} path="/about"></Route>
+<Route component={Contact} path="/contact"></Route>
+
+
 </div>
-
-
-
-
-</HashRouter>, document.getElementById('menu'));
+</HashRouter>, document.getElementById('app'));
+*/
